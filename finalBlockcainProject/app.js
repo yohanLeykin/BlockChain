dc498fa8fa7c19c5e0d8522b9fe330920d90b659
yohanLeykin <yohan.leykin@gmail.com>
2024-03-15 16:27:32 +0200

    Change file path so connect to metamask button would work

--- finalBlockcainProject/app.js
+++ finalBlockcainProject/app.js
@@ -3,9 +3,10 @@ import path from 'path';
 const app = express();
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join("C:/Workspace/blockchainLabs/finalBlockcainProject" + "/index.html"));
-})
+    const filePath = path.join(process.cwd(), "index.html"); // Using process.cwd() to get the current working directory
+    res.sendFile(filePath);
+});
 
 const server = app.listen(5000);
 const portNumber = server.address().port;
-console.log(`port is open on ${portNumber}`);
+console.log(`Port is open on ${portNumber}`);
